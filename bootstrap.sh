d0d9d7734b09207a6396c6c8cfc5774d6bb4d109
Keith Resar <keith.resar@422long.com>
2018-02-21 12:44:20 -0600

    -

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,27 +1,27 @@
 
 
 # create global template
-oc create -f template.yaml -n openshift
+#oc create -f template.yaml -n openshift
 
 # create project
-oc new-project letsencrypt
+#oc new-project letsencrypt
 
 # register letsencrypt user id
-brew install dehydrated
-echo CONTACT_EMAIL=${EMAIL} > my_config
-dehydrated -f my_config --register --accept-terms
+#brew install dehydrated
+#echo CONTACT_EMAIL=${EMAIL} > my_config
+#dehydrated -f my_config --register --accept-terms
 
 # create secret
 oc create secret generic letsencrypt-creds \
      --from-file=account-key=`find ~/rht-ansible/openshiftdelivers/ocp_letsencrypt/files/letsencrypt_key_*.pem`
 
 # Add permissions to the letsencrypt user we just created
-oc create -f letsencrypt-clusterrole.yaml
+#oc create -f letsencrypt-clusterrole.yaml
 
 
 # create app in our current project
 oc new-app --template=letsencrypt -p LETSENCRYPT_CONTACT_EMAIL=${EMAIL}
 
-oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:test2:letsencrypt
+oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:demo:letsencrypt
 
 
